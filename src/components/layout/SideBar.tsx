61e311a48b89267a36501b67e3b9ed7bdba83242
szymon <szymonfularczyk@o2.pl>
2025-10-25 22:04:02 +0200

    Add Avatar component and Resizable UI components; update SideBar to include user avatar and settings

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,9 +1,17 @@
-import { ChevronsLeft, ChevronsRight } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronsLeft,
+  ChevronsRight,
+  Settings,
+} from "lucide-react";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import React, { useEffect } from "react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import type { User } from "@/types/user";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Separator } from "../ui/separator";
+import useLogout from "@/hooks/auth/useLogout";
 
 interface SideBarProps {
   user: User;
@@ -12,6 +20,8 @@ interface SideBarProps {
 }
 
 const SideBar = ({ user, isOpen, setIsOpen }: SideBarProps) => {
+  const { mutate: logout } = useLogout();
+
   function handleClick(event: React.MouseEvent) {
     event.preventDefault();
     setIsOpen(!isOpen);
@@ -41,10 +51,26 @@ const SideBar = ({ user, isOpen, setIsOpen }: SideBarProps) => {
             <Popover>
               <PopoverTrigger className="w-full" asChild>
                 <Button
-                  className="w-full flex items-center justify-between p-[0.1rem]"
+                  className="w-full flex items-center justify-between  p-[0.1rem] group"
                   variant={"ghost"}
                 >
-                  <div>{user.username}'s Interact app</div>
+                  <div className="flex items-center gap-2 flex-1 min-w-0">
+                    <Avatar className="p-[3px]">
+                      <AvatarFallback>
+                        {user.username.slice(0, 2)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <p className="text-sm font-medium tracking-tight text-start min-w-0 overflow-hidden whitespace-nowrap text-ellipsis">
+                      {user.firstName}'s Interact
+                    </p>
+                    <Button
+                      variant={"ghost"}
+                      size={"icon-sm"}
+                      className="hidden group-hover:flex shrink-0"
+                    >
+                      <ChevronDown />
+                    </Button>
+                  </div>
                   <Tooltip delayDuration={200}>
                     <TooltipTrigger asChild>
                       <Button
@@ -64,8 +90,44 @@ const SideBar = ({ user, isOpen, setIsOpen }: SideBarProps) => {
                   </Tooltip>
                 </Button>
               </PopoverTrigger>
-              <PopoverContent className="w-[17rem] bg-red ml-[0.1rem]">
-                Content
+              <PopoverContent className="w-[17rem] bg-red ml-[0.1rem] p-0">
+                <div className="flex flex-col text- gap-1">
+                  <div className="flex flex-col gap-2 px-3 py-2">
+                    <div className="flex gap-2">
+                      <Avatar className="size-10">
+                        <AvatarFallback>
+                          {user.username.slice(0, 2)}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <p className="text-[0.85rem]">
+                          {user.firstName} {user.lastName}
+                        </p>
+                        <p className="text-muted-foreground text-xs">
+                          Since {user.createdAt.split("T")[0]}
+                        </p>
+                      </div>
+                    </div>
+                    <Button
+                      variant={"outline"}
+                      size={"sm"}
+                      className="w-fit text-[0.85rem] h-7"
+                    >
+                      <Settings />
+                      Settings
+                    </Button>
+                  </div>
+                  <Separator className="w-full" />
+                  <div className="flex flex-col gap-1 px-3 py-2 text-[0.85rem]">
+                    <p className="text-muted-foreground">{user.email}</p>
+                    <button
+                      onClick={() => logout()}
+                      className="text-sm w-fit p-0 m-0 hover:underline cursor-pointer"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
               </PopoverContent>
             </Popover>
           )}
