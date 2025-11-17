2f91a0a04f167f95e3bc05bc984cdfb226d03c7c
szymon <szymonfularczyk@o2.pl>
2025-11-17 21:15:39 +0100

    Refactor _root route to restore user authentication loader and integrate SideBar component

--- src/routes/_root.tsx
+++ src/routes/_root.tsx
@@ -1,33 +1,35 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import "./index.css";
-import InDevelopment from "@/components/InDevelopment";
+import type { User } from "@/types/user";
+import SideBar from "@/components/layout/SideBar";
+import { useState } from "react";
 
 export const Route = createFileRoute("/_root")({
-  // loader: async () => {
-  //   const response = await fetch("/api/auth/current");
+  loader: async () => {
+    const response = await fetch("/api/auth/current");
 
-  //   if (response.status === 401) {
-  //     throw redirect({ to: "/login" });
-  //   }
+    if (response.status === 401) {
+      throw redirect({ to: "/login" });
+    }
 
-  //   const user = (await response.json()) as User;
-  //   return { user };
-  // },
+    const user = (await response.json()) as User;
+    return { user };
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  // const { user } = Route.useLoaderData();
+  const { user } = Route.useLoaderData();
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <main className="test flex h-screen">
-      <InDevelopment />
-      {/* <SideBar user={user} isOpen={isOpen} setIsOpen={setIsOpen} />
+      <SideBar user={user} isOpen={isOpen} setIsOpen={setIsOpen} />
       <div
         className={`w-full transition-[margin] duration-300 ${isOpen ? "ml-[18rem]" : "ml-0"}`}
       >
         <Outlet />
-      </div> */}
+      </div>
     </main>
   );
 }
